a0684875e3b9a27c68c8bf58cfc94ec12555cd7e
Simon <Simon@DESKTOP-KEIOBOC.fritz.box>
2023-12-02 14:05:51 +0100

    Added no shock strategy.
    With this strategy there is no shock applied to the economy

---
+++ cbdc/src/cbdc/strategies/NoShock.java
@@ -0,0 +1,42 @@
+package cbdc.strategies;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import benchmark2.StaticValues;
+import benchmark2.agents.Bank;
+import benchmark2.agents.CapitalFirm;
+import benchmark2.agents.ConsumptionFirm;
+import benchmark2.agents.Households;
+import benchmark2.strategies.ShockStrategy;
+import jmab2.agents.MacroAgent;
+import jmab2.population.MacroPopulation;
+import jmab2.simulations.MacroSimulation;
+import jmab2.stockmatrix.Item;
+import net.sourceforge.jabm.Population;
+import net.sourceforge.jabm.SimulationController;
+import net.sourceforge.jabm.agent.Agent;
+import net.sourceforge.jabm.strategy.AbstractStrategy;
+
+public class NoShock extends AbstractStrategy implements ShockStrategy{
+	
+	public void performShock(){
+		
+	}
+	
+	
+	
+	@Override
+	public byte[] getBytes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void populateFromBytes(byte[] content, MacroPopulation pop) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
